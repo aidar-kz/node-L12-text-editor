734f02bbaf296c050226fb0145ef1f4b0207a438
Aydeke <aydeke2@gmail.com>
2022-06-27 21:25:21 +0600

    Add mongoose slug generator

---
+++ models/Documents.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
+
+mongoose.plugin(slug);
+const documentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  timeCreated: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  slug: {
+    type: String,
+    slug: "title",
+    unique: true,
+    slug_padding_size: 2,
+  },
+});
+
+module.exports = mongoose.model("Document", documentSchema);
--- routes/documents.js
+++ routes/documents.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const Document = require("../models/Documents.js");
 
-router.get("/documents/new", (req, res) => {
+router.get("/new", (req, res) => {
   res.render("doc-new", { title: "Новый документ" });
 });
 
-router.post("/documents", (req, res) => {
-  console.log(req.body);
+router.post("/", (req, res) => {
+  const { title, text } = req.body;
+  const document = new Document({ title, text });
+  document
+    .save()
+    .then((document) => res.redirect(`documents/${document.slug}`))
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
